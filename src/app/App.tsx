14cabd2fc1f2c63425f1de2a3c137cf18b15cc48
SHDGMSQ <sh.dg.msq@mail.ru>
2022-06-20 21:45:18 +0300

    commit after 15 without login (need check all reducers with tests)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,7 +6,7 @@ import {TodolistsList} from '../features/TodolistsList/TodolistsList';
 import {useAppSelector} from './hooks';
 import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar';
 
-//need to check tests for all reducers
+//need to check tests
 
 export const App: React.FC<PropsType> = ({demo = false}) => {
 
